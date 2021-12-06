d52aadf5ed54434f78f58201a95f68f77668de17
Mrugank Dake <mrugankdake@gmail.com>
2021-12-06 00:26:32 -0500

    assignment 4
    Minor changes

--- assignment4/direction_selectivity_assignment.m
+++ assignment4/direction_selectivity_assignment.m
@@ -135,7 +135,7 @@ duration = 1000; % ms
 t = 0:deltaT:duration-deltaT;
 x = zeros(size(t));
 x(100) = 1;
-tau = 50; % ms
+tau = 25; % ms
 [f1, f2] = lp_filter(x, t, deltaT, tau);
 
 fig7 = figure();
@@ -145,7 +145,7 @@ plot(t, f2, 'DisplayName', 'f_2')
 xlabel('time (ms)')
 legend()
 
-%% 3)
+%% 3 a)
 deltaX = 1/120;
 x_x = -2:deltaX:2;
 x_y = -2:deltaX:2;
@@ -155,146 +155,97 @@ x(241, 241, 1) = 1; % Impulse line
 tau = 25;
 [f1, f2] = space_time_filter(x, t, deltaT, tau);
 
-%%
 sig = 0.1;
 sf = 4;
-evenFilt = exp(-(x_x.^2)./(2*sig^2)) .* cos(2*pi*sf*x_x);
-oddFilt = exp(-(x_x.^2)./(2*sig^2)) .* sin(2*pi*sf*x_x);
-integral = sum(evenFilt.^2 + oddFilt.^2);
-evenFilt = evenFilt / integral;
-oddFilt = oddFilt / integral;
-
-%%
-oddFast = zeros(length(x_x), length(x_y), length(t));
-oddSlow = zeros(length(x_x), length(x_y), length(t));
-evenSlow = zeros(length(x_x), length(x_y), length(t));
-evenFast = zeros(length(x_x), length(x_y), length(t));
-
-for tt = 1:length(t)
-    oddFast(:, :, tt) = conv2(f1(:, :, tt), oddFilt', 'same');
-    oddSlow(:, :, tt) = conv2(f2(:, :, tt), oddFilt', 'same');
-    evenSlow(:, :, tt) = conv2(f2(:, :, tt), evenFilt', 'same');
-    evenFast(:, :, tt) = conv2(f1(:, :, tt), evenFilt', 'same');
-end
-%%
+[evenFilt, oddFilt] = gabor_filter(x_x, sig, sf);
+[oddFastlr, oddSlowlr, evenFastlr, evenSlowlr] = ...
+    sum_diff(f1, f2, oddFilt, evenFilt);
 fig8 = figure();
-subplot(2,2,1)
-imagesc(squeeze(oddFast(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Odd Fast')
+plot_func(oddFastlr, oddSlowlr, evenFastlr, evenSlowlr, 'Odd Fast lr', 'Odd Slow lr', ...
+    'Even Fast lr', 'Even Slow lr', 'lr')
 
-subplot(2,2,2)
-imagesc(squeeze(oddSlow(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Odd Slow')
-
-subplot(2,2,3)
-imagesc(squeeze(evenFast(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Even Fast')
-
-subplot(2,2,4)
-imagesc(squeeze(evenSlow(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Even Slow')
+[oddFastud, oddSlowud, evenFastud, evenSlowud] = ...
+    sum_diff(f1, f2, oddFilt', evenFilt');
+fig9 = figure();
+plot_func(oddFastud, oddSlowud, evenFastud, evenSlowud, 'Odd Fast ud', 'Odd Slow ud', ...
+    'Even Fast ud', 'Even Slow ud', 'ud')
 
 %% b)
-leftEven = oddFast + evenSlow;
-leftOdd = -oddSlow + evenFast;
-rightEven = -oddFast + evenSlow;
-rightOdd = oddSlow + evenFast;
+[leftEven, leftOdd, rightEven, rightOdd, leftEnergy, rightEnergy] = ...
+    motion_energy(oddFastlr, oddSlowlr, evenFastlr, evenSlowlr);
 
-%%
-fig9 = figure();
-subplot(2,2,1)
-imagesc(squeeze(leftEven(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Left Even')
-
-subplot(2,2,2)
-imagesc(squeeze(leftOdd(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Left Odd')
-
-subplot(2,2,3)
-imagesc(squeeze(rightEven(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Right Even')
-
-subplot(2,2,4)
-imagesc(squeeze(rightOdd(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Right Odd')
-
-
-%% c)
-leftEnergy = leftEven.^2 + leftOdd.^2; 
-rightEnergy = rightEven.^2. + rightOdd.^2;
+[upEven, upOdd, downEven, downOdd, upEnergy, downEnergy] = ...
+    motion_energy(oddFastud, oddSlowud, evenFastud, evenSlowud);
 
 fig10 = figure();
-subplot(2,2,1)
-imagesc(squeeze(leftEnergy(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Left Energy')
+plot_func(leftEven, leftOdd, rightEven, rightOdd, 'Left Even', 'Left Odd', ...
+    'Right Even', 'Right Odd', 'lr')
 
-subplot(2,2,2)
-imagesc(squeeze(rightEnergy(:, 241, :))')
-colormap(gray)
-xticks([1, 121, 241, 361, 481])
-xticklabels([-2, -1, 0, 1, 2])
-yticks(0:100:1000)
-xlabel('Visual angle (deg)')
-ylabel('time (ms)')
-title('Right Energy')
+fig11 = figure();
+plot_func(upEven, upOdd, downEven, downOdd, 'Up Even', 'Up Odd', ...
+     'Down Even', 'Down Odd', 'ud')
+
+%% c)
+fig12 = figure();
+plot_func(leftEnergy, rightEnergy, upEnergy, downEnergy, 'Left Energy', ...
+    'Right Energy', 'Up Energy', 'Down Energy', 'energy')
 
 %% d)
 
+dir_amps = [0, pi/2]; % for left-right, and up-down
+phase_shifts = [4, -4]; % positive is left or down, negative is right or up
+% for dir_amp = dir_amps
+%     for phase_shift = phase_shifts
+amp_sinusoid = 1;
+phase = 0;
+phase_shift = 2*pi/125;
+sinusoid_x = zeros(length(x_x), length(x_y), length(t));
+sf = 30;
 
+for tt = 1:1000
+    phase = phase + 2*pi/125;%blah_blah;
+    sinusoid_x(:, :, tt) = get_grating(x_x, 1, phase, sf, 'lr');
+end
+
+deltaX = 1/120;
+x_x = -2:deltaX:2;
+deltaT = 1; % ms
+duration = 1000; % ms
+t = 0:deltaT:duration-deltaT;
+tau = 25; % ms
+
+[f1, f2] = space_time_filter(sinusoid_x, t, deltaT, tau);
+
+[oddFastlr, oddSlowlr, evenFastlr, evenSlowlr] = ...
+    sum_diff(f1, f2, oddFilt, evenFilt);
+[oddFastud, oddSlowud, evenFastud, evenSlowud] = ...
+    sum_diff(f1, f2, oddFilt', evenFilt');
+[leftEven, leftOdd, rightEven, rightOdd, leftEnergy, rightEnergy] = ...
+    motion_energy(oddFastlr, oddSlowlr, evenFastlr, evenSlowlr);
+[downEven, downOdd, upEven, upOdd, downEnergy, upEnergy] = ...
+    motion_energy(oddFastud, oddSlowud, evenFastud, evenSlowud);
+%%
+figure();
+pp = 241;
+plot(t, squeeze(leftEnergy(pp, pp, :)), 'DisplayName', 'leftEnergy');
+hold on;
+plot(t, squeeze(leftEven(pp, pp, :)), 'DisplayName', 'leftEven');
+plot(t, squeeze(leftOdd(pp, pp, :)), 'DisplayName', 'leftOdd');
+legend()
+%title([num2str(blah_blah), ' ', num2str(per_freq)])
+% figure();
+% imagesc(sinusoid_x(:, :, 1));
+% colormap(gray)
+% figure();
+% imagesc(sinusoid_x(:, :, 100));
+% colormap(gray)
+%hold on;
+%plot(res(:, 10), 'r');
+
+%% 4)
+
+[leftEnergyNorm, rightEnergyNorm, upEnergyNorm, downEnergyNorm] = ...
+    energy_norm(leftEnergy, rightEnergy, upEnergy, downEnergy);
 
 %% Functions
 function y1 = output(x, t, deltaT, tau)
@@ -302,7 +253,6 @@ function y1 = output(x, t, deltaT, tau)
     for tt = 1:length(t) - 1
         deltaY1 = (deltaT/tau) * (-y1(tt) + x(tt));
         y1(tt + 1) = y1(tt) + deltaY1;
-        %y1Save(tt) = y1;
     end
 end
 
@@ -344,6 +294,121 @@ function [f1, f2] = space_time_filter(x, t, deltaT, tau)
         f2(:, :, tt) = y(:, :, tt, 5) - y(:, :, tt, 7);
     end
     
+end
+
+function [evenFilt, oddFilt] = gabor_filter(x, sig, sf)
+    evenFilt = exp(-(x.^2)./(2*sig^2)) .* cos(2*pi*sf*x);
+    oddFilt = exp(-(x.^2)./(2*sig^2)) .* sin(2*pi*sf*x);
+    integral = sum(evenFilt.^2 + oddFilt.^2);
+    evenFilt = evenFilt / integral;
+    oddFilt = oddFilt / integral;
+end
+
+function [oddFast, oddSlow, evenFast, evenSlow] = ...
+    sum_diff(f1, f2, oddFilt, evenFilt)
+    [lx, ly, lt] = size(f1);
+    oddFast = zeros(lx, ly, lt);
+    oddSlow = zeros(lx, ly, lt);
+    evenSlow = zeros(lx, ly, lt);
+    evenFast = zeros(lx, ly, lt);
+    
+    for tt = 1:lt
+        oddFast(:, :, tt) = conv2(f1(:, :, tt), oddFilt, 'same');
+        oddSlow(:, :, tt) = conv2(f2(:, :, tt), oddFilt, 'same');
+        evenSlow(:, :, tt) = conv2(f2(:, :, tt), evenFilt, 'same');
+        evenFast(:, :, tt) = conv2(f1(:, :, tt), evenFilt, 'same');
+    end
+end
+
+function [Even1, Odd1, Even2, Odd2, Energy1, Energy2]...
+    = motion_energy(oddFast, oddSlow, evenFast, evenSlow)  
+    Even1 = oddFast + evenSlow;
+    Odd1 = -oddSlow + evenFast;
+    Even2 = -oddFast + evenSlow;
+    Odd2 = oddSlow + evenFast;
+    Energy1 = Even1.^2 + Odd1.^2; 
+    Energy2 = Even2.^2. + Odd2.^2;
+end
+
+function [leftEnergyNorm, rightEnergyNorm, upEnergyNorm, downEnergyNorm] = ...
+    energy_norm(leftEnergy, rightEnergy, upEnergy, downEnergy)
+    sumEnergy = leftEnergy + rightEnergy + upEnergy + downEnergy;
+
+    leftEnergyNorm = leftEnergy ./ (sumEnergy + stdev^2);
+    rightEnergyNorm = rightEnergy ./ (sumEnergy + stdev^2);
+    upEnergyNorm = upEnergy ./ (sumEnergy + stdev^2);
+    downEnergyNorm = downEnergy ./ (sumEnergy + stdev^2);
+end
+
+function plot_func(a, b, c, d, a_title, b_title, c_title, d_title, direc)
+    if strcmp(direc, 'lr')
+        a = squeeze(a(241, :, :))';
+        b = squeeze(b(241, :, :))';
+        c = squeeze(c(241, :, :))';
+        d = squeeze(d(241, :, :))';
+    elseif strcmp(direc, 'ud')
+        a = squeeze(a(:, 241, :))';
+        b = squeeze(b(:, 241, :))';
+        c = squeeze(c(:, 241, :))';
+        d = squeeze(d(:, 241, :))';
+    elseif strcmp(direc, 'energy')
+        a = squeeze(a(241, :, :))';
+        b = squeeze(b(241, :, :))';
+        c = squeeze(c(:, 241, :))';
+        d = squeeze(d(:, 241, :))';
+    end
+    subplot(2,2,1)
+    imagesc(a)
+    colormap(gray)
+    xticks([1, 121, 241, 361, 481])
+    xticklabels([-2, -1, 0, 1, 2])
+    yticks(0:100:1000)
+    xlabel('Visual angle (deg)')
+    ylabel('time (ms)')
+    title(a_title)
+
+    subplot(2,2,2)
+    imagesc(b)
+    colormap(gray)
+    xticks([1, 121, 241, 361, 481])
+    xticklabels([-2, -1, 0, 1, 2])
+    yticks(0:100:1000)
+    xlabel('Visual angle (deg)')
+    ylabel('time (ms)')
+    title(b_title)
+
+    subplot(2,2,3)
+    imagesc(c)
+    colormap(gray)
+    xticks([1, 121, 241, 361, 481])
+    xticklabels([-2, -1, 0, 1, 2])
+    yticks(0:100:1000)
+    xlabel('Visual angle (deg)')
+    ylabel('time (ms)')
+    title(c_title)
+
+    subplot(2,2,4)
+    imagesc(d)
+    colormap(gray)
+    xticks([1, 121, 241, 361, 481])
+    xticklabels([-2, -1, 0, 1, 2])
+    yticks(0:100:1000)
+    xlabel('Visual angle (deg)')
+    ylabel('time (ms)')
+    title(d_title)
+end
+
+function sinewave2D = get_grating(x_x, amp, phase, sf, ori)
+    %n = 101;
+    [X,Y] = meshgrid(x_x .* sf);
+    if strcmp(ori, 'lr')
+        sinewave2D = sin(amp * X + phase);
+    elseif strcmp(ori, 'ud')
+        sinewave2D = sin(amp * Y + phase);
+    end
+%     figure(1)
+%     imagesc(sinewave2D)
+%     axis equal;axis off;colormap(gray);
 end
 % function space_time_filter(x, t, deltaT, tau)
 %     n = [3,5,5,7];
